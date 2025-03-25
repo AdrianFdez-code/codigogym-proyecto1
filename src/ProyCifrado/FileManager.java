51084a06ebd5606be2ac7ce750218cf18b620163
Adrian Fdez. <adrifg901@gmail.com>
2025-03-24 19:35:29 -0600

    Se actualizó el menú

---
+++ src/ProyCifrado/FileManager.java
@@ -0,0 +1,11 @@
+package ProyCifrado;
+
+public class FileManager {
+    public String readFile(String filePath) {
+        // File reading logic
+        return null;
+    }
+    public void writeFile(String content, String filePath) {
+        // Logic for writing a file
+    }
+}
--- src/ProyCifrado/Main.java
+++ src/ProyCifrado/Main.java
@@ -3,5 +3,8 @@ package ProyCifrado;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido al proyecto Cifrado César!!");
+        Menu.opciones();
     }
+
+
 }
